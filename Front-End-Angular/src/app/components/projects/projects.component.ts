148486a8a4d95a22a383f0127e03bc4677f8bd37
Manuel <trapecioinv@gmail.com>
2022-04-27 22:48:08 -0300

    Seccion Proyectos: Binding realizado con Template Binding; Binding de attr para href y src.

--- Front-End-Angular/src/app/components/projects/projects.component.ts
+++ Front-End-Angular/src/app/components/projects/projects.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { PortfolioDataService } from 'src/app/services/portfolio-data.service';
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.css']
+  styleUrls: ['./projects.component.css'],
 })
 export class ProjectsComponent implements OnInit {
+  apiUrl: string = this.portfolioData.apiUrl;
+  projectsData: any;
+  projectsList: any;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private portfolioData: PortfolioDataService) {
+    this.portfolioData.getData().subscribe((data) => {
+      this.projectsData = data.projects;
+      this.projectsList = data.projectsList;
+    });
   }
 
+  ngOnInit(): void {}
 }
