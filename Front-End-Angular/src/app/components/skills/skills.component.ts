3740da4c5daf5b359f5432e91c829c8b3fb9e8a2
Manuel <trapecioinv@gmail.com>
2022-04-27 22:38:12 -0300

    Seccion Skills: Template Binding, Style Binding para las barras de progreso

--- Front-End-Angular/src/app/components/skills/skills.component.ts
+++ Front-End-Angular/src/app/components/skills/skills.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { PortfolioDataService } from 'src/app/services/portfolio-data.service';
 
 @Component({
   selector: 'app-skills',
   templateUrl: './skills.component.html',
-  styleUrls: ['./skills.component.css']
+  styleUrls: ['./skills.component.css'],
 })
 export class SkillsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  apiUrl: string = this.portfolioData.apiUrl;
+  skillsData: any;
+  frontData: any;
+  backData: any;
+  softData: any;
+  constructor(private portfolioData: PortfolioDataService) {
+    this.portfolioData.getData().subscribe((data) => {
+      this.skillsData = data.skills;
+      this.frontData = data.frontend;
+      this.backData = data.backend;
+      this.softData = data.soft;
+    });
   }
 
+  ngOnInit(): void {}
 }
