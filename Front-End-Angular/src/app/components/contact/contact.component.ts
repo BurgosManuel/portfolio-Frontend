75a6cd4c937eca7887bd22e36ab3e0ebc513e563
Manuel <trapecioinv@gmail.com>
2022-04-27 23:04:40 -0300

    Seccion Contacto: Agregado Template Binding

--- Front-End-Angular/src/app/components/contact/contact.component.ts
+++ Front-End-Angular/src/app/components/contact/contact.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { PortfolioDataService } from 'src/app/services/portfolio-data.service';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.css']
+  styleUrls: ['./contact.component.css'],
 })
 export class ContactComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  contactData: any;
+  constructor(private portfolioData: PortfolioDataService) {
+    this.portfolioData.getData().subscribe((data) => {
+      this.contactData = data.contact;
+    });
   }
 
+  ngOnInit(): void {}
 }
