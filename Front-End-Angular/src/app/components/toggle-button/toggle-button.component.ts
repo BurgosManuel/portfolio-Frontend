96076457fe3037af7c62abcf4d4a60af5cfed93f
Manuel <trapecioinv@gmail.com>
2022-04-28 19:06:43 -0300

    Funcion para cambiar icono de boton al clickear LISTO; EventEmitter LISTO; Falta vincular con Sidebar.

--- Front-End-Angular/src/app/components/toggle-button/toggle-button.component.ts
+++ Front-End-Angular/src/app/components/toggle-button/toggle-button.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-toggle-button',
   templateUrl: './toggle-button.component.html',
-  styleUrls: ['./toggle-button.component.css']
+  styleUrls: ['./toggle-button.component.css'],
 })
 export class ToggleButtonComponent implements OnInit {
 
-  constructor() { }
+  @Output() onToggleButton: EventEmitter<any> = new EventEmitter;
+  showSidebar: boolean = true;
+  constructor() {}
 
-  ngOnInit(): void {
+  toggleSidebar() {
+    this.showSidebar = !this.showSidebar
+    this.onToggleButton.emit(this.showSidebar)
   }
-
+  ngOnInit(): void {}
 }
