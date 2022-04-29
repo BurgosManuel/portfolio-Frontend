65c9c7737bb1db6d0119797c05fe6d1b44d12460
Manuel <trapecioinv@gmail.com>
2022-04-28 21:00:05 -0300

    Funcionalidad Ocultar/Mostrar implementada y vinculada al sidebar + comentarios

--- Front-End-Angular/src/app/components/toggle-button/toggle-button.component.ts
+++ Front-End-Angular/src/app/components/toggle-button/toggle-button.component.ts
@@ -6,14 +6,28 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
   styleUrls: ['./toggle-button.component.css'],
 })
 export class ToggleButtonComponent implements OnInit {
+  @Output() onToggleButton: EventEmitter<any> = new EventEmitter();
 
-  @Output() onToggleButton: EventEmitter<any> = new EventEmitter;
-  showSidebar: boolean = true;
-  constructor() {}
+  // Creamos una propiedad que almacene el min-width para que se considere la sidebar 'Abierta'
+  minWidth = window.matchMedia('(min-width: 1200px)');
+  // Booleano que nos muestra si el botón será para abrir o cerrar la barra.
+  isOpen: boolean;
 
-  toggleSidebar() {
-    this.showSidebar = !this.showSidebar
-    this.onToggleButton.emit(this.showSidebar)
+  constructor() {
+    // Determinamos si el botón -al instanciarse- se usará para abrir o cerrar el sidebar.
+    this.minWidth.matches ? (this.isOpen = true) : (this.isOpen = false);
   }
+
+  // Método para cambiar el estado de 'isOpen', se bindeará a través de (click) y emitirá un evento personalizado al componente que lo contiene (navbar)
+  toggleSidebar(): void {
+    this.isOpen = !this.isOpen;
+    this.onToggleButton.emit();
+  }
+
+  // Método que va a checkear el tamaño de la pantalla y modificar el estado de 'isOpen' en base a esto. Será bindeado a través del evento (window: resize), para que se ejecute cada vez que cambia el tamaño de la ventana.
+  loadButton(): void {
+    this.minWidth.matches ? (this.isOpen = true) : (this.isOpen = false);
+  }
+
   ngOnInit(): void {}
 }
