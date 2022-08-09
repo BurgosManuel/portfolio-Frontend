aada28efc64c569a9d6f8d0d33fd340466f44b2d
Manuel <trapecioinv@gmail.com>
2022-08-09 18:23:59 -0300

    FIX Perdida de autorizacion en recarga

--- Front-End-Angular/src/app/app.component.ts
+++ Front-End-Angular/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { TokenStorageService } from './services/token-storage.service';
+import { UserService } from './services/user.service';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +8,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  constructor(private tokenStorage: TokenStorageService, private userStatus: UserService){}
+
+  ngOnInit() {
+    this.tokenStorage.getToken() ? this.userStatus.setUserStatus(true) : this.userStatus.setUserStatus(false);
+  }
 }
