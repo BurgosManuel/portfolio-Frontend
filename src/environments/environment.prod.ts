be0ae460303a44f68940e60ea5afd8bc7fe2a4c1
Manuel <trapecioinv@gmail.com>
2022-08-14 22:11:29 -0300

    Update Front URL Enviroment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   baseUrl: 'https://portfolio-manuel-backend.herokuapp.com',
   authApi: 'https://portfolio-manuel-backend.herokuapp.com/api/auth',
-  frontUrl: 'https://portfolio-manuel-b6adb.web.app/portfolio/'
+  frontUrl: 'https://burgosmanuel.ar/portfolio/'
 };
