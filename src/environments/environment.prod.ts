5924e01fd9a5f4a45398489a2b325e76f46f321c
Manuel <trapecioinv@gmail.com>
2022-08-16 11:27:19 -0300

    new api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  baseUrl: 'https://portfolio-manuel-backend.herokuapp.com',
-  authApi: 'https://portfolio-manuel-backend.herokuapp.com/api/auth',
+  baseUrl: 'https://api.burgosmanuel.ar',
+  authApi: 'https://api.burgosmanuel.ar/api/auth',
   frontUrl: 'https://burgosmanuel.ar/portfolio/'
 };
