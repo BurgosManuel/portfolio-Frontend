fed343a20a8a491842fec3bb71e285dd2d9c220d
Burgos Manuel <83843908+BurgosManuel@users.noreply.github.com>
2023-11-06 20:20:51 -0300

    Update portfolio-data.service.ts

--- src/app/services/portfolio-data.service.ts
+++ src/app/services/portfolio-data.service.ts
@@ -7,6 +7,7 @@ import { TokenStorageService } from './token-storage.service';
 const httpOptions = {
   headers: new HttpHeaders({
     'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*'
   }),
 };
 
